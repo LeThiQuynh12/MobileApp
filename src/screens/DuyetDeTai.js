9d8a2e925404bd76ee86e69907ed8073462bf2b9
LeThiQuynh12 <lequynh4431r@gmail.com>
2025-02-13 22:40:24 +0700

    MobileApp

---
+++ src/screens/DuyetDeTai.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+const DuyetDeTai = () => {
+  const navigation = createStackNavigator();
+  return (
+    <View>
+      <Text>DuyetDeTai</Text>
+    </View>
+  );
+};
+
+export default DuyetDeTai;
+
+const styles = StyleSheet.create({});
